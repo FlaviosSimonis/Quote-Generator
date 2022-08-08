cf154122bd505a6c7683a434f7fc3e0fd72f75a6
FlaviosSimonis <flavios.simonis@gmail.com>
2022-08-09 02:41:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//API METHOD
+let apiQuotes = [];
+
+//Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+//Show new quote
+function newQuote() {
+    loading();
+    //Pick a random quote form apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+    //Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // Check quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    //Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
+    //console.log(quote);
+}
+//
+// Get Quotes form api
+async function getQuotes() {
+    loading();
+    const apiUrl='https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+        //console.log(apiQuotes[12]);
+    } catch (error) {
+        //alert(error)
+        //catch error here
+    }
+}
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On load
+getQuotes();
+
+
+
+//LOCAL METHOD
+//Show new quote
+// function newQuote() {
+//     //Pick a random quote form localQuotes array
+//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//     console.log(quote);
+// }
+// newQuote();
